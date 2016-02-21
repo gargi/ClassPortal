df76c06cdee84ce0210f95c59e2da46af9548549
kpalani5 <kpalani5@ncsu.edu>
2016-02-21 12:36:17 -0500

    Fixing more links 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,4 @@ User.create(name: 'super', email: 'super@gmail.com', password: 'super', type: 's
 User.create(name: 'admin', email: 'admin@gmail.com', password: 'admin', type: 'admin')
 User.create(name: 'instructor', email: 'instructor@gmail.com', password: 'instructor', type: 'instructor')
 User.create(name: 'student', email: 'student@gmail.com', password: 'student', type: 'student')
-Course.create(course_number: 'CSC517', title: 'OODD', description: 'Object Oriented Design and Development',start_date: '06/01/2016',
-              end_date: '07/05/2016',status: 'active',user_id: '3')
-Enrollment.create(grade: '-',course_id: '1',user_id: '4', status: 'enrolled')
-Material.create(course_id: '1',content: 'Welcome to CSC 517')
-Material.create(course_id: '1',content: 'Classes start from 06/01/2016')
+
