29ce07d54e2ef2560290acf50f83c20e8d74c587
kpalani5 <kpalani5@ncsu.edu>
2016-02-26 20:56:51 -0500

    SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(name: 'super', email: 'super@gmail.com', password: 'super', type: 'superadmin')
-User.create(name: 'admin', email: 'admin@gmail.com', password: 'admin', type: 'admin')
-User.create(name: 'instructor', email: 'instructor@gmail.com', password: 'instructor', type: 'instructor')
-User.create(name: 'student', email: 'student@gmail.com', password: 'student', type: 'student')
+User.create(name: 'super', email: 'super@ncsu.edu', password: 'super', type: 'superadmin')
+User.create(name: 'admin', email: 'admin@ncsu.edu', password: 'admin', type: 'admin')
+User.create(name: 'instructor', email: 'instructor@ncsu.edu', password: 'instructor', type: 'instructor')
+User.create(name: 'student', email: 'student@ncsu.edu', password: 'student', type: 'student')
 
